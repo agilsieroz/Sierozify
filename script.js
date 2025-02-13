5d884adee39eeeb8946919fcf4c99d115bdc8076
agilsieroz <agillsirodj@gmail.com>
2025-02-13 17:16:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Animasi Font Tambahan
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedTitle = document.querySelector('.animated-title');
+    animatedTitle.style.opacity = '1';
+  });
